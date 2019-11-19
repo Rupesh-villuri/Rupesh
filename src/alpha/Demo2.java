c3e790048ba72b26293269c93ce277f67cb7c3d4
Rupesh <rupesh.villuri@gmail.com>
2019-11-19 23:13:23 +0530

    Architect in timezone created some new classes

---
+++ src/alpha/Demo2.java
@@ -0,0 +1,15 @@
+package alpha;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New class and Code added in new branch");
+		int i=10;
+		int A[]= {5,6,8};
+		System.out.println(A[2]);
+		System.out.println(i);
+
+	}
+
+}
---
+++ src/beta/demo3.java
@@ -0,0 +1,13 @@
+package beta;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Cretaed another class in other package");
+		
+		//making some code writings
+
+	}
+
+}
